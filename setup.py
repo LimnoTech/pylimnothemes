6e861a3e250d19dd8435c44f90389161a34049d7
Sarah Jordan <sjordan@limno.com>
2021-10-25 11:21:34 -0500

    update packages to make library work

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='pylimnothemes',
 	version = 0.1,
 	description='consistent matplotlib styling for LimnoTech style guide',
 	author = 'Sarah Jordan', 
+	url = 'https://github.com/LimnoTech/pylimnothemes.git',
 	author_email = 'sjordan@limno.com', 
 	packages = ['pylimnothemes'],
 	install_requires=['matplotlib'])
