07b948781e605b6f9bdcdfb3af1de576bc4982d2
Sarah Jordan <sjordan@limno.com>
2021-10-25 15:07:07 -0500

    update default theme

--- pylimnothemes/theme_limno.py
+++ pylimnothemes/theme_limno.py
@@ -7,21 +7,29 @@ from .limno_pal import limno_pal
 from .limno_cols import limno_cols
 
 
-def theme_limno(palette='main'):
+def theme_limno(palette='main', reverse=False):
 
 	# font
 	mpl.rcParams['font.family'] = 'sans-serif'
 	mpl.rcParams['font.sans-serif'] = ['Calibri']
+
+	# font colors
 	mpl.rcParams['axes.titlecolor'] = '#4D4D4D'
 	mpl.rcParams['xtick.color'] = '#999999'
 	mpl.rcParams['xtick.labelcolor'] = '#999999'
 	mpl.rcParams['ytick.labelcolor'] = '#999999'
 	mpl.rcParams['ytick.color'] = '#999999'
+	mpl.rcParams['axes.labelcolor'] = '#999999'
+	mpl.rcParams['text.color'] = '#999999'
 
-	cpal = limno_pal(palette)
-	print(cpal)
-	print(type(cpal))
-	test = cycler(color=['red', 'green', 'blue'])
+	# background color
+	mpl.rcParams["figure.facecolor"] = 'white'
+	mpl.rcParams["axes.facecolor"] = 'white'
+	mpl.rcParams["savefig.facecolor"] = 'white'
 
-    # color palette 
+	# spine color 
+	mpl. rcParams['axes.edgecolor']  = '#999999'
+
+	# set color palette
+	cpal = limno_pal(palette, reverse=reverse)
 	mpl.rcParams['axes.prop_cycle'] = cycler(color=cpal)
